a44ea5677632017e3893ef3fd856315057392d7c
ArthurJuma98 <arthurjuma98@gmail.com>
2024-08-21 11:50:55 +0300

    delete job

--- backend/controllers/jobController.js
+++ backend/controllers/jobController.js
@@ -67,7 +67,29 @@ exports.updateJob = async (req, res) => {
         job.jobType = jobType || job.jobType;
 
         const updatedJob = await job.save();
-        res.status(200).json(updatedJob)
+        res.status(200).json(updatedJob);
+
+    } catch (error) {
+        res.status(500).json({ message: error.message });
+    }
+};
+
+exports.deleteJob = async (req, res) => {
+    try {
+        const job = await Job.findById(req.params.id);
+
+        if (!job) {
+            return res.status(404).json({ message: 'job not found!' })
+        }
+
+        //validate if logged-in user is the job poster
+
+        if (job.company.toString() !== req.user._id.toString()) {
+            return res.status(403).json({ message: 'Not Authorized to delete this job!' });
+        }
+
+        await job.remove();
+        res.status(200).json({ message: 'job has been removed!' });
     } catch (error) {
         res.status(500).json({ message: error.message });
     }
