5ba17d451a5fed3540a23a9f83653f650b1b3aca
ArthurJuma98 <arthurjuma98@gmail.com>
2024-08-26 17:31:12 +0300

    server update

--- backend/server.js
+++ backend/server.js
@@ -3,6 +3,7 @@ const connectDB  = require('./config/db');
 const dotenv = require('dotenv');
 const authRoutes = require('./routes/authRoutes');
 const jobRoutes = require('./routes/jobRoutes');
+const applicationRoutes = require('./routes/applicationRoutes');
 
 dotenv.config();
 
@@ -14,6 +15,7 @@ app.use(express.json());
 
 app.use('/api/auth', authRoutes);
 app.use('/api/jobs', jobRoutes);
+app.use('/api/applications', applicationRoutes);
 
 const PORT = process.env.PORT || 5000;
 
