918f445b3338adc391e4640ecc66030b8407723d
ArthurJuma98 <arthurjuma98@gmail.com>
2024-08-29 20:22:33 +0300

    admin controller update

--- backend/controllers/adminController.js
+++ backend/controllers/adminController.js
@@ -31,3 +31,16 @@ exports.getAllJobs = async (req, res) => {
         res.status(500).json({ message: error.message });
     }
 };
+
+exports.getJobDetails = async (req, res) => {
+    try {
+        const job = await Job.findById(req.params.id).populate('company', 'name email');
+        
+        if (!job) {
+            return res.status(404).json({ message: 'Job not found!' });
+        }
+        res.status(200).json(job);
+    } catch (error) {
+        res.status(500).json({ message: error.message });
+    }
+};
