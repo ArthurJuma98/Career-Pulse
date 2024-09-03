fb95e440dd273f9ae7a4bea933c35c4063522c25
ArthurJuma98 <arthurjuma98@gmail.com>
2024-09-03 14:22:28 +0300

    admin routes update

--- backend/routes/adminRoutes.js
+++ backend/routes/adminRoutes.js
@@ -5,15 +5,26 @@ const { protect, admin } = require('../middleware/authMiddleware');
 
 const router = express.Router();
 
+//manage jobs
+
 router.get('/dashbord', getDashboardData, protect, admin);
 router.get('/jobs', protect, admin, getAllJobs);
 router.get('/jobs/:id', protect, admin, getJobDetails);
 router.put('/jobs/:id', protect, admin, updateJob);
 router.delete('/jobs/:id', protect, admin, deleteJob);
 
+//manage applications
+
 router.get('/applications', protect, admin, getAllApplications);
 router.get('/applications', protect, admin, getApplicationDetails);
 router.put('/applcations', protect, admin, updateApplicationStatus);
 router.delete('/applications', protect, admin, deleteApplicaton);
+
+
+//manage users
+router.get('/users', protect, admin, getAllUsers);
+router.get('/users/:id', protect, admin, getUserDetails);
+router.put('/users/:id', protect, admin, updateUser);
+router.delete('/users/:id', protect, admin, deleteUser);
 
 module.exports = router;
