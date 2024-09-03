f74c974e7f254a210ad7481544a488d7cb090596
ArthurJuma98 <arthurjuma98@gmail.com>
2024-09-03 15:08:58 +0300

    get user details admin

--- backend/controllers/adminController.js
+++ backend/controllers/adminController.js
@@ -147,3 +147,16 @@ exports.getAllUsers = async (req, res) => {
         res.status(500).json({ message: error.message });
     }
 };
+
+exports.getUserDetails = async (req, res) => {
+    try {
+        const user = await User.findBy(req.params.id).select('-password');
+        
+        if (!user) {
+            res.status(404).json({ message: 'User not found!'});
+        }
+        res.status(200).json(user);
+    } catch (error) {
+        res.status(500).json({ message: error.message });
+    }
+};
