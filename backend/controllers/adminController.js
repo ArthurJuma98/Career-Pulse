92260c1538792e0a42cfbacc3b95bcfea0a0d966
ArthurJuma98 <arthurjuma98@gmail.com>
2024-09-03 15:01:36 +0300

    admin controller get users

--- backend/controllers/adminController.js
+++ backend/controllers/adminController.js
@@ -138,3 +138,12 @@ exports.updateApplicationStatus = async (req, res) => {
         res.status(500).json({ message: error.message });
     }
 };
+
+exports.getAllUsers = async (req, res) => {
+    try {
+        const users = await User.find().select('-password');
+        res.status(200).json(users);
+    } catch (error) {
+        res.status(500).json({ message: error.message });
+    }
+};
