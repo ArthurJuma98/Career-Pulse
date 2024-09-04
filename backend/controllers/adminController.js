e1c9614d2d1ea5fdb6c8497fe7fbf4149723bc9c
ArthurJuma98 <arthurjuma98@gmail.com>
2024-09-04 17:46:47 +0300

    delete user admin

--- backend/controllers/adminController.js
+++ backend/controllers/adminController.js
@@ -176,3 +176,16 @@ exports.updateUser = async (req, res) => {
         res.status(500).json({ message: error.message });
     }
 };
+
+exports deleteUser = async (req, res) => {
+    try {
+        const user = await User.findByIdAndDelete(req.params.id);
+
+        if (!user) {
+            return res.status(404).json({ message: 'User not found!' });
+        }
+        res.status(200).json({ message: 'User deleted sucessfully!' });
+    } catch (error) {
+        res.status(500).json({ message: error.message });
+    }
+};
