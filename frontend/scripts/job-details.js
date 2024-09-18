e2b61b6a9708525c508176bc9a0fa97756175fd1
ArthurJuma98 <arthurjuma98@gmail.com>
2024-09-18 16:09:28 +0300

    job details script

--- frontend/scripts/job-details.js
+++ frontend/scripts/job-details.js
@@ -9,4 +9,33 @@ document.addEventListener('DOMContentLoaded', async () => {
     document.getElementById('job-company').textContent = job.company.name;
     document.getElementById('job-location').textContent = job.location;
     document.getElementById('job-description').textContent = job.description;
-})
+
+    document.getElementById('apply-btn').addEventListener('click', async () => {
+        const userId = localStorage.getItem('userId');
+
+        if (!userId) {
+            alert('You need to log in to apply!');
+            return;
+        }
+
+        const application = {
+            jobId,
+            userId,
+            coverLetter: 'add your cover letter here'
+        };
+
+        const applyResponse = await fetch('api/applications', {
+            method: 'post',
+            headers: {
+                'Content-Type': 'application/json',
+            },
+            body: JSON.stringify(application)
+        });
+
+        if (applyResponse.ok) {
+            alert('application submitted succesfully')
+        } else {
+            alert('failed to submit application, please try again!')
+        }
+    });
+});
